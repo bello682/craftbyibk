c410b903b399a9afdfd9557382d7913117816fcb
bello682 <belloadetayo14@gmail.com>
2026-03-04 19:58:39 +0100

    added final contact, privacy and terms pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,7 +78,7 @@ export default function RootLayout({
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3730534578729256"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
         <ReduxProvider>
           <ModalProvider>{children}</ModalProvider>
